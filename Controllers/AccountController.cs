90dab43563075ed915f511bb7e5dbe486352f6a1
Edvin <eduardo.vinicius2009@hotmail.com>
2022-05-11 03:32:07 -0300

    Configuration - Email service and Image upload

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,9 +3,12 @@ using Blog.Extensions;
 using Blog.Models;
 using Blog.Services;
 using Blog.ViewModels;
+using Blog.ViewModels.Accounts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SecureIdentity.Password;
+using System.Text.RegularExpressions;
 
 namespace Blog.Controllers
 {
@@ -15,6 +18,7 @@ namespace Blog.Controllers
         [HttpPost("v1/accounts")]
         public async Task<IActionResult> Post(
             [FromBody] RegisterViewModel model,
+            [FromServices] EmailService emailService,
             [FromServices] BlogDataContext context)
         {
             if (!ModelState.IsValid)
@@ -34,6 +38,8 @@ namespace Blog.Controllers
             {
                 await context.Users.AddAsync(user);
                 await context.SaveChangesAsync();
+
+                emailService.Send(user.Name, user.Email, "Welcome to the blog!", $"Your password is <strong>{password}</strong>");
 
                 return Ok(new ResultViewModel<dynamic>(new
                 {
@@ -82,5 +88,46 @@ namespace Blog.Controllers
             }
         }
 
+        [Authorize]
+        [HttpPost("v1/accounts/upload-image")]
+        public async Task<IActionResult> UploadImage(
+            [FromBody] UploadImageViewModel model,
+            [FromServices] BlogDataContext context)
+        {
+            var filename = $"{Guid.NewGuid().ToString()}.jpg";
+            var data = new Regex(@"^data:image\/[a-z]+;base64,").Replace(model.Base64Image, "");
+            var bytes = Convert.FromBase64String(data);
+
+            try
+            {
+                await System.IO.File.WriteAllBytesAsync($"wwwroot/images/{filename}", bytes);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ResultViewModel<string>("05X04 - Internal server fault."));
+            }
+
+            var user = await context
+                .Users
+                .FirstOrDefaultAsync(x => x.Email == User.Identity.Name);
+
+            if (user == null)
+                return NotFound(new ResultViewModel<User>("User not found!"));
+
+            user.Image = $"https://localhost:0000/images/{filename}";
+
+            try
+            {
+                context.Users.Update(user);
+                await context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ResultViewModel<string>("05X04 - Internal server fault."));
+            }
+
+            return Ok(new ResultViewModel<string>("Image successfully altered!", null));
+        }
+        
     }
 }
--- ViewModels/Categories/EditorCategoryViewModel.cs
+++ ViewModels/Categories/EditorCategoryViewModel.cs
@@ -1,13 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Blog.ViewModels
+namespace Blog.ViewModels.Categories
 {
     public class EditorCategoryViewModel
     {
         [Required]
         [StringLength(40, MinimumLength = 3, ErrorMessage = "The name must be at least 3 characters long and have a maximum of 40 characters.")]
         public string Name { get; set; }
-        
+
         [Required]
         public string Slug { get; set; }
     }
